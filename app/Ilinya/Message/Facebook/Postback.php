ca86ef86b7f2e7a59b6aff9d0fbafca0bef7907d
kennCK <kennettecanales@gmail.com>
2020-08-17 23:29:15 +0800

    update

--- app/Ilinya/Message/Facebook/Postback.php
+++ app/Ilinya/Message/Facebook/Postback.php
@@ -88,7 +88,7 @@ class Postback
                         $this->bot->reply($this->room->roomMenuStart(), false);
                         $this->bot->reply($this->room->roomMenu(), false);
                         break;
-                    case strtolower('food_&_beverage') :
+                    case strtolower('food&beverage') :
                         $this->bot->reply($this->post->foodAndBeverageMenu(), false);
                         break;
                     case strtolower('food & beverage') :
