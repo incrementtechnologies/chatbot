f168660faa265dba3bafb834d23e1ba040113079
kind-soul07 <ziekind07.com>
2020-09-08 11:17:19 +0800

    bot configuration

---
+++ database/migrations/2020_09_04_044817_create_bot_configuration.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateBotConfiguration extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('bot_configurations', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->bigInteger('account_id');
+            $table->string('payload');
+            $table->longText('payload_value');
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('logs');
+    }
+}
