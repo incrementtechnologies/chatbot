a983f98f747d568651c4bccae503244c69e90a98
Kennette Canales <kennettecanales@gmail.com>
2020-09-20 11:37:52 +0800

    update

--- app/Ilinya/Response/Facebook/PostbackResponse.php
+++ app/Ilinya/Response/Facebook/PostbackResponse.php
@@ -100,8 +100,9 @@ class PostbackResponse{
         return $response;
     }
     public function start(){
-        $this->user();
-        $title =  "Such a great day to get in touch with you, ".$this->user->getFirstName().". I'm Sean, to better help you with your inquiry, please select the following options below:";
+        // $this->user();
+        // $title =  "Such a great day to get in touch with you, ".$this->user->getFirstName().". I'm Sean, to better help you with your inquiry, please select the following options below:";
+        $title =  "Such a great day to get in touch with you. I'm Sean, to better help you with your inquiry, please select the following options below:";
         $subtitle = "Kindly click the buttons to navigate.";
         $imageUrl = "http://ilinya.com/wp-content/uploads/2017/08/cropped-logo-copy-copy.png";
         $menus= array(
@@ -121,7 +122,7 @@ class PostbackResponse{
         return $response;
     }
     public function foodAndBeverageMenu(){
-        $this->user();
+        // $this->user();
         $title =  "Food & Beverage";
         $buttons=[];
         $menus= array( 
@@ -139,7 +140,7 @@ class PostbackResponse{
         return $response;
     }
     public function inquiry(){
-        $this->user();
+        // $this->user();
         $title = "For more Concerns and Inquiries.";
         $imageUrl = "http://ilinya.com/wp-content/uploads/2017/08/cropped-logo-copy-copy.png";
         $menus = array(
