e636589f6a42bcb85d0c5eb44adb318126e32f47
kennck <kennettecanales@gmail.com>
2021-08-05 20:46:01 +0800

    update

--- app/Ilinya/API/SheetController.php
+++ app/Ilinya/API/SheetController.php
@@ -7,27 +7,30 @@ class SheetController
 {
 
 
-    public static function getSheetContent($arr) {        
-        $url = $arr[0];
-        $num = (int)$arr[1];
-        $curl = new Curl();
-         $sheetData = $curl->get($url ,true);
-         $entries   = $sheetData["feed"]["entry"];
-         $categories = array();
-         $headers = array();
-         for ($i=0; $i < $num; ++$i) { 
-             array_push($headers ,$entries[$i]['content']['$t']);
-         }
-        
-        for ($j= sizeof($headers); $j < sizeof($entries) ; $j+=$num) { 
-            $object = array();
-            for ($k=0; $k < sizeof($headers) ; $k++) {
-                if(($j + $k) < sizeof($entries)){
-                    $object[$headers[$k]]= $entries[$j+$k]['content']['$t'];
-                }
-            }
-            array_push($categories, $object);   
-        }
-        return $categories;
+  public static function getSheetContent($arr) {        
+    $url = $arr[0];
+    $num = (int)$arr[1];
+    $curl = new Curl();
+    $sheetData = $curl->get($url ,true);
+    $entries   = $sheetData["feed"]["entry"];
+    $categories = array();
+    $headers = array();
+    for ($i=0; $i < $num; ++$i) { 
+      array_push($headers ,$entries[$i]['content']['$t']);
     }
+
+    if($entries){
+      for ($j= sizeof($headers); $j < sizeof($entries) ; $j+=$num) { 
+        $object = array();
+        for ($k=0; $k < sizeof($headers) ; $k++) {
+          if(($j + $k) < sizeof($entries)){
+            $object[$headers[$k]]= $entries[$j+$k]['content']['$t'];
+          }
+        }
+        array_push($categories, $object);   
+      }            
+    }
+
+    return $categories;
+  }
 }
